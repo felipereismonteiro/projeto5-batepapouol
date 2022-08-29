4b65a6dd88bb7d179d4853aa1afce89cca0ab841
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-08-29 00:59:33 -0300

    completo

--- js/js.js
+++ js/js.js
@@ -62,7 +62,7 @@ function chegou(res) {
 
         if (novoArray[i].text == "entra na sala..." || novoArray[i].text == "sai da sala...") {
             novoArray[i].to = "Entrou"
-        } else if(novoArray[i].type == "private_message") {
+        } else if(novoArray[i].type == "private_message" && novoArray[i].to === server.name) {
             lista.innerHTML += 
             `<li>
                 <div class="Reservadamente">
